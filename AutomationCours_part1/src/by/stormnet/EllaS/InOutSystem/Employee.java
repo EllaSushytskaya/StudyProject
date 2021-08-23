1e864675bf7c8e6e8c610b4bcbbb1a5b3e579f5f
EllaSushytskaya <ella.sushytskaya@gmail.com>
2021-08-23 23:14:04 +0300

    update Jurnal.java

--- AutomationCours_part1/src/by/stormnet/EllaS/InOutSystem/Employee.java
+++ AutomationCours_part1/src/by/stormnet/EllaS/InOutSystem/Employee.java
@@ -1,37 +1,24 @@
 package by.stormnet.EllaS.InOutSystem;
 
-import by.stormnet.EllaS.InOutSystem.IDcard;
-
 public abstract class Employee {
 	protected String name = "";
 	protected String last_name = "";
 
-
 	public Employee(String name, String last_name) {
 		this.name = name;
 		this.last_name = last_name;
-
 	}
 
 	static class idCard {
 		public static void getIDcard() {
 		}
 	}
-
-	public enum Status {
-		InOffice,
-		InOfficeWithoutCard,
-		OutOffice;
-	}
 }
 
 
-//	public static String getStatus(String Status) {
-//		for (int i = 0; i < Status.values().length; i++) {
-//			if (Status.values()[i] == Status.valueOf("InOffice")) {
-//				return Status.values()[i].name();
-//			}
-//		}
+
+
+
 
 
 
--- AutomationCours_part1/src/by/stormnet/EllaS/InOutSystem/Jurnal.java
+++ AutomationCours_part1/src/by/stormnet/EllaS/InOutSystem/Jurnal.java
@@ -2,13 +2,32 @@ package by.stormnet.EllaS.InOutSystem;
 
 public class Jurnal {
 
-	public static void main(String[] args) {
-		int[] Employee = new int[15];
 
-
+	public static String getEnter() {
+		boolean idcard;
+		int[] quantityEmployee = new int[15];
+		for (int i = 0; i < quantityEmployee.length; i++) {
+			if (idcard=true){
+				return "Employee find";
+			}
+		}
+		return "Employee come on the button";
 	}
 
-}
+
+		public enum Status {
+			InOffice,
+			InOfficeWithoutCard,
+			OutOffice;
+		}
+	}
+
+
+
+
+
+
+
 
 
 
